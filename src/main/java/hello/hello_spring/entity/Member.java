ae1013aabc27445fe1ff95357482e8d3e6303386
2-jun9 <jungu269@gmail.com>
2024-09-06 16:12:32 +0900

    fix: 변경 일부 반영

--- src/main/java/hello/hello_spring/entity/Member.java
+++ src/main/java/hello/hello_spring/entity/Member.java
@@ -1,10 +1,9 @@
 package hello.hello_spring.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Member {
